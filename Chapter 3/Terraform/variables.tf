f7dcca167632bb17d19a9fb31ac6b3705a87b161
AnkeetC <115153160+AnkeetC@users.noreply.github.com>
2023-10-04 18:09:51 +0530

    Update variables.tf

--- Chapter 3/Terraform/variables.tf
+++ Chapter 3/Terraform/variables.tf
@@ -1,39 +1,39 @@
 ### VPC ###
 variable "aws-region" {
-  type = "string"
+  type = string
   description = "AWS region"
 }
 variable "aws-availability-zones" {
-  type = "string"
+  type = string
   description = "AWS zones"
 }
 variable "vpc-cidr" {
-  type = "string"
+  type = string
   description = "VPC CIDR"
 }
 variable "vpc-name" {
-  type = "string"
+  type = string
   description = "VPC name"
 }
 
 ### EC2 ###
 variable "autoscaling-group-minsize" {
-  type = "string"
+  type = string
   description = "Min size of the ASG"
 }
 variable "autoscaling-group-maxsize" {
-  type = "string"
+  type = string
   description = "Max size of the ASG"
 }
 variable "autoscaling-group-image-id" {
-  type="string"
+  type= string
   description = "EC2 AMI identifier"
 }
 variable "autoscaling-group-instance-type" {
-  type = "string"
+  type = string
   description = "EC2 instance type"
 }
 variable "autoscaling-group-key-name" {
-  type = "string"
+  type = string
   description = "EC2 ssh key name"
 }
